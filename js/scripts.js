93bb0fa85287c992a08ed5b40cd857b7939d97b0
Isaacg94 <7248zack@gmail.com>
2019-08-25 15:49:39 +0300

    add hover effect to portfolio section using jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,62 @@ $(document).ready(function(){
     $("#productManagementImage").slideDown('1500');
   });
 });
+
+
+
+$(document).ready(function(){
+  $("#featuredWork").mouseover(function(){
+    $("#overlay").show();
+  }).mouseout(function(){
+    $("#overlay").hide();
+  });
+});
+$(document).ready(function(){
+  $("#featuredWork1").mouseover(function(){
+    $("#overlay1").show();
+  }).mouseout(function(){
+    $("#overlay1").hide();
+  });
+});
+$(document).ready(function(){
+  $("#featuredWork2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#featuredWork3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#featuredWork4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+});
+$(document).ready(function(){
+  $("#featuredWork5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+});
+$(document).ready(function(){
+  $("#featuredWork6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+});
+$(document).ready(function(){
+  $("#featuredWork7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+});
