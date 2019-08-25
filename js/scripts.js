674e642a757f794abf7082863021afd84f4e7265
Isaacg94 <7248zack@gmail.com>
2019-08-25 11:31:38 +0300

    add effects to development and product-management icons

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,25 @@ $(document).ready(function(){
     $("#designImage").slideDown('1500');
   });
 });
+
+$(document).ready(function(){
+  $("#developmentImage").click(function(){
+    $("#developmentImage").slideDown('1500').hide('1000');
+    $("#developmentText").show('1500');
+  });
+  $("#developmentText").click(function(){
+    $("#developmentText").slideUp('1500');
+    $("#developmentImage").slideDown('1500');
+  });
+});
+
+$(document).ready(function(){
+  $("#productManagementImage").click(function(){
+    $("#productManagementImage").slideDown('1500').hide('1000');
+    $("#productManagementText").show('1500');
+  });
+  $("#productManagementText").click(function(){
+    $("#productManagementText").slideUp('1500');
+    $("#productManagementImage").slideDown('1500');
+  });
+});
